8eaf9f894fa2552f4e816b434b2098c5665108d3
macprotego <50519322+macprotego@users.noreply.github.com>
2023-05-06 13:22:07 +0800

    added table.js file

--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,16 +7,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, projID, claimID, currency, status) {
+  return { name, projID, claimID, currency, status };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('P001', 'C001', 2000, 'Pending'),
+  createData('P002', 'C002', 860, 'Approved'),
+  createData('P003', 'C003', 250, 'Rejected'),
+ 
 ];
 
 export default function BasicTable() {
@@ -26,10 +25,10 @@ export default function BasicTable() {
         <TableHead>
           <TableRow>
             <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right">Project ID</TableCell>
+            <TableCell align="right">Claim ID</TableCell>
+            <TableCell align="right">Currency</TableCell>
+            <TableCell align="right">Status</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -41,10 +40,10 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.projID}</TableCell>
+              <TableCell align="right">{row.claimID}</TableCell>
+              <TableCell align="right">{row.currency}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
             </TableRow>
           ))}
         </TableBody>
