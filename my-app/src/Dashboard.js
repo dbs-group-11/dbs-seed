79483d7d7158178f576caba436e7c15f6334b4dc
macprotego <50519322+macprotego@users.noreply.github.com>
2023-05-06 12:31:15 +0800

    added dashboard, footer, and table files

---
+++ my-app/src/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const dashboard=()=>{
+    return(
+        <div className="dashboard-title">
+            <ul className="dashboard-ul">
+                <h1>Dashboard</h1>
+            </ul>
+            <button onClick={''}> Create Claim</button>
+        </div>
+    )
+}
+
+export default dashboard;
